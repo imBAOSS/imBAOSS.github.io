ea4661f814d9b1d132f339ba0618b9bf2076e942
imbaoss <baole6989@gmail.com>
2017-05-04 01:10:56 -0700

    Add iframe with video

--- assets/js/main.js
+++ assets/js/main.js
@@ -234,4 +234,26 @@
 
 	});
 
+	$("[data-media]").ready(function() {
+    var $this = $(this);
+    var videoUrl = $this.attr("data-media");
+    var popup = $this.attr("href");
+    var $popupIframe = $(popup).find("iframe");
+
+    $popupIframe.attr("src", videoUrl);
+
+    $this.closest(".page").addClass("show-popup");
+	});
+
+	$(".popup").on("click", function(e) {
+	    e.preventDefault();
+	    e.stopPropagation();
+
+	    $(".page").removeClass("show-popup");
+	});
+
+	$(".popup > iframe").on("click", function(e) {
+	    e.stopPropagation();
+});
+
 })(jQuery);
